b5657031fe7c23c1c86fa0ca39d327edd4030232
Maria-crypto-coder <77939046+Maria-crypto-coder@users.noreply.github.com>
2023-07-04 01:01:18 -0300

    Parte 2

---
+++ ProyectoPanaderia2/ProyectoPanaderia/PedidosException.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ayala
+ * Date: 17/5/2023
+ * Time: 01:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ProyectoPanaderia
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	class PedidosException : Exception
+	{
+	    public PedidosException(string message) : base(message) 
+	    {
+		}
+	}
+}
